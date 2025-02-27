f8a5b7307202206b689a874bc442ce1d6720a49e
srushtinb <srushtiicse@gmail.com>
2025-02-27 22:31:52 +0530

    updated

---
+++ Class/Conversionandoperation.js
@@ -0,0 +1,33 @@
+//let score ="33abc" ;output string string number NaN(not a number)
+//let score = null  ;output will be 0 & type-object(obeject,object,number,0)
+//let score = undefined  ;output will NaN (undefined,undefined,number,NaN)
+//let score = true ; output will be 0 type-boolean(boolean,boolean,number,1)
+
+
+// console.log(typeof score); 
+// console.log(typeof(score)); //as a method
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = "sonu"
+// let booleanIsLoggedin = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedin); 
+//OP:true for 1
+ //OP:false for 0
+ //Op:false for ""
+ //op:true for " "/"sonu"
+
+
+
+ let someNumber =33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+
+
+
+
