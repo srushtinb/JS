0cceb1c4324c26c5b7d5ad64951ab76abe3d2931
srushtinb <srushtiicse@gmail.com>
2025-02-27 19:36:27 +0530

    Updated

---
+++ Class/test.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail="srush@gmail.com"
+var accountPassword="12345"
+accountCity ="shimoga"
+accountCity="manglore"
+//accountId=2//not allowed
+console.log(accountId);
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
